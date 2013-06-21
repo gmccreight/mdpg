071c8f62b470962959b680509f4058e548d3f082
Gordon McCreight <gordon@mccreight.com>
2013-06-20 19:27:15 -0700

    tiny refactorings

--- spec/wnp/user_vm_spec.rb
+++ spec/wnp/user_vm_spec.rb
@@ -7,17 +7,19 @@ require "minitest/autorun"
 describe Wnp::UserVm do
 
   before do
-    @data = get_data()
-    @user = Wnp::User.new(@data, 1)
-    @env = Wnp::Env.new(@data, @user)
+    @user = Wnp::User.new(get_data(), 1)
+    @env = Wnp::Env.new(get_data(), @user)
     @user_vm = Wnp::UserVm.new(@env, @user)
   end
 
   it "should list the page ids and names sorted by name" do
-    @data.set "userdata-1-page-ids", [1,2]
+    user = create_user 1
 
     create_page 1, :name => "zebra-training"
     create_page 2, :name => "alaska-crab"
+
+    user.add_page 1
+    user.add_page 2
 
     assert_equal [[2, "alaska-crab"], [1, "zebra-training"]], @user_vm.page_ids_and_names_sorted_by_name()
   end
