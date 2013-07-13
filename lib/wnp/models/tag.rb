a926bbf59078d0a53f137904f94144563b373689
Gordon McCreight <gordon@mccreight.com>
2013-07-13 12:48:34 -0700

    add page_ids to tag model

--- lib/wnp/models/tag.rb
+++ lib/wnp/models/tag.rb
@@ -2,9 +2,21 @@ module Wnp::Models
 
   class Tag < Base
 
-    attr_accessor :name, :type, :associated_ids
+    attr_accessor :name, :page_ids, :user_ids
+
+    def add_page_id id
+      self.page_ids = ((page_ids || []) + [id]).sort.uniq.sort
+    end
+
+    def remove_page_id id
+      self.page_ids = ((page_ids || []) - [id]).sort.uniq.sort
+    end
 
     private
+
+      def unique_id_indexes
+        [:name]
+      end
 
       def validates?
       end
