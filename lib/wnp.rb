60d5183d022891463f2b69a2e8d10d544e64fd1a
Gordon McCreight <gordon@mccreight.com>
2013-07-14 14:24:33 -0700

    do a more dynamic file requiring

--- lib/wnp.rb
+++ lib/wnp.rb
@@ -1,19 +1,6 @@
-require 'wnp/data'
-require "wnp/models/base"
-require "wnp/models/group"
-require "wnp/models/page"
-require "wnp/models/tag"
-require "wnp/models/user"
-require 'wnp/env'
-require 'wnp/token'
-require 'wnp/page_vm'
-require 'wnp/token'
-require 'wnp/user'
-require 'wnp/services/object_tags'
-require 'wnp/services/user_groups'
-require 'wnp/services/user_page_permission'
-require 'wnp/services/user_page_tags'
-require 'wnp/services/user_pages'
+Dir[File.dirname(__FILE__) + '/wnp/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/wnp/models/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/wnp/services/*.rb'].each {|file| require file }
 
 module Wnp
 
