db4d92539590eac0587201fb2333c17a72dc5419
Gordon McCreight <gordon@mccreight.com>
2013-08-17 00:39:17 -0700

    do not store datakey, and use key in filename

--- lib/mdpg/data_store.rb
+++ lib/mdpg/data_store.rb
@@ -16,12 +16,10 @@ class DataStore < Struct.new(:data_dir_or_memory)
         end
       end
     end
-    remove_datakey data
     data
   end
 
   def set key, data
-    add_datakey_for_easy_file_searching data, key
     if data_dir_or_memory == :memory
       set_in_memory_value(key, data)
     else
@@ -33,37 +31,16 @@ class DataStore < Struct.new(:data_dir_or_memory)
   end
 
   def virtual_delete key
-    deleted_suffix = "__deleted"
+    set(key + "__deleted", get(key))
+
     if data_dir_or_memory == :memory
-      val = @data[key]
-      @data[key + deleted_suffix] = val
       @data.delete key
     else
-      deleted_path = full_path_for_key(key) + deleted_suffix
-      FileUtils.mv(full_path_for_key(key), deleted_path)
+      File.delete(full_path_for_key(key))
     end
   end
 
   private
-
-    def add_datakey_for_easy_file_searching data, key
-      datakey_value = "_datakey_#{key}"
-
-      if data.class == Hash
-        data[:_datakey] = datakey_value
-      elsif data.class == Array
-        data.push datakey_value
-      end
-
-    end
-
-    def remove_datakey data
-      if data.class == Hash
-        data.delete :_datakey
-      elsif data.class == Array
-        data.pop
-      end
-    end
 
     def get_in_memory_value key
       @data ||= {}
@@ -76,18 +53,12 @@ class DataStore < Struct.new(:data_dir_or_memory)
     end
 
     def full_path_for_key key
-      data_dir_or_memory() + "/" + directory_for_key(key) + "/" +
-        filename_for_key(key)
+      data_dir_or_memory() + "/" + directory_for_key(key) + "/" + key
     end
 
     def directory_for_key key
       digest = digest_of_key key
       digest[0..1]
-    end
-
-    def filename_for_key key
-      digest = digest_of_key key
-      digest[2..-1]
     end
 
     def digest_of_key key
