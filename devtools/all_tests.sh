ed470eab60d4b280724eee3286b39d3cab7a14ea
Gordon McCreight <gordon@mccreight.com>
2013-10-27 11:04:59 -0700

    Add two more style tests

--- devtools/all_tests.sh
+++ devtools/all_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# style tests
 ./devtools/over_78_chars.sh
+./devtools/trailing_whitespace.sh
+./devtools/tabs.sh
+
 perf=1 rake
 karma start --single-run
---
+++ devtools/trailing_whitespace.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+find . -type f | egrep "\.(coffee|css|haml|md|rb)" | xargs egrep -n ".*[ ]+$"
