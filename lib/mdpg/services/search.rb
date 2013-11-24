9509badf3844ddae21ef3040c63be360f3c17dfa
Gordon McCreight <gordon@mccreight.com>
2013-11-23 17:27:12 -0800

    Use the search query parser

--- lib/mdpg/services/search.rb
+++ lib/mdpg/services/search.rb
@@ -3,12 +3,15 @@ class Search
   def initialize user
     @user_pages = UserPages.new(user)
     @user_page_tags = UserPageTags.new(user, nil)
+    @search_query = SearchQueryParser.new()
   end
 
   def search query
-    names = @user_pages.pages_with_names_containing_text(query)
-    texts = @user_pages.pages_with_text_containing_text(query)
-    tags = @user_page_tags.search(query)
+    @search_query.query = query
+    search_string = @search_query.search_string()
+    names = @user_pages.pages_with_names_containing_text(search_string)
+    texts = @user_pages.pages_with_text_containing_text(search_string)
+    tags = @user_page_tags.search(search_string)
     {names: names, texts:texts, tags:tags}
   end
 
