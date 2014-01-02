baa9bc5c81ffe23cb63961991463ded2257906e8
Gordon McCreight <gordon@mccreight.com>
2014-01-02 12:56:21 -0800

    add a comment

--- lib/mdpg.rb
+++ lib/mdpg.rb
@@ -1,3 +1,5 @@
+# For some reason, the production app crashes if you don't have this line:
+# Perhaps look into removing it after upgrading to ruby 2.1 on the server.
 require "mdpg/models/base"
 
 ([""] + %w{models/ services/ viewmodels/}).each do |x|
