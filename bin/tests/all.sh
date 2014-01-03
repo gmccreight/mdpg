cf0d0d99e9e71d82d5dae109bdf28a37ff445498
Gordon McCreight <gordon@mccreight.com>
2014-01-02 22:54:13 -0800

    Revert "adding stricter checking of non hash rockets"
    This reverts commit a6498c00198dec52229aaf6baad6f5898aea6a7b.

--- bin/tests/all.sh
+++ bin/tests/all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # style tests
-./bin/tests/not_hash_rockets.sh
 ./bin/tests/over_78_chars.sh
 ./bin/tests/trailing_whitespace.sh
 ./bin/tests/tabs.sh
--- bin/tests/not_hash_rockets.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-grep -r "[\"'a-z]:\s*[\"'a-z]" lib spec app.rb | grep -v "tags:"
