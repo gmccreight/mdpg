1a56e15b81bc2c8996e1bd45857c100382b099e1
Gordon McCreight <gordon@mccreight.com>
2014-01-04 00:04:54 -0800

    refactor the combos generator

--- lib/home_row_char_combos_generator.rb
+++ lib/home_row_char_combos_generator.rb
@@ -1,14 +1,14 @@
 class HomeRowCharCombosGenerator
 
   def initialize
-    @_combos = {}
+    @_already_generated_combos = {}
   end
 
-  def next_char_combo length
+  def next_uniq_char_combo length
     while true
-      chars = _rand_right_left_repeat_combo length
-      if ! @_combos.has_key?(chars)
-        @_combos[chars] = true
+      chars = _rand_right_left_right_left_repeat_char_combo length
+      if ! @_already_generated_combos.has_key?(chars)
+        @_already_generated_combos[chars] = true
         return chars
       end
     end
@@ -16,17 +16,12 @@ class HomeRowCharCombosGenerator
 
   private
 
-    def _rand_right_left_repeat_combo length
-      right_chars = %w{h j k l}
-      left_chars = %w{a s d f g}
+    def _rand_right_left_right_left_repeat_char_combo length
+      keys = [%w{a s d f g}, %w{h j k l}]
 
       chars = ""
-      (1..length).to_a.each do |x|
-        if x % 2 == 1
-          chars << right_chars.sample()
-        else
-          chars << left_chars.sample()
-        end
+      (1..length).each do |x|
+        chars << keys[(x % 2)].sample()
       end
 
       chars
