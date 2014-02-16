e8134ead297c4fda07d5c5812dd96f9689e1935b
Gordon McCreight <gordon@mccreight.com>
2014-02-16 15:36:12 -0800

    add tag renaming spec

---
+++ spec/sinatra/tag_spec.rb
@@ -0,0 +1,46 @@
+require File.expand_path '../sinatra_helper.rb', __FILE__
+
+describe "tag" do
+
+  before do
+    $data_store = get_memory_datastore()
+
+    @user = User.create name:"Jordan",
+      email:"jordan@example.com", password:"cool"
+    user_pages = UserPages.new @user
+    @page = UserPages.new(@user).create_page name:"a-good-page",
+      text:"I wish I had something *interesting* to say!"
+  end
+
+  def add_tag user, page_name, tag_name
+    post "/p/#{page_name}/tags", {:text => tag_name}.to_json,
+      authenticated_session(user)
+  end
+
+  def rename_tag user, original_name, new_name
+    post "/t/#{original_name}/rename", {:new_name => new_name},
+      authenticated_session(user)
+  end
+
+  describe "renaming" do
+
+    before do
+      add_tag @user, "a-good-page", "new-1"
+
+      @other_page = UserPages.new(@user).create_page name:"other-page",
+        text:"nothing really"
+      add_tag @user, "other-page", "new-1"
+    end
+
+    it "should rename the tag if no other with name exists" do
+      rename_tag @user, "new-1", "new-2"
+      follow_redirect_with_authenticated_user!(@user.reload)
+      assert_equal ["new-2"],
+        UserPageTags.new(@user, @page.reload).get_tags()
+      assert_equal ["new-2"],
+        UserPageTags.new(@user, @other_page.reload).get_tags()
+    end
+
+  end
+
+end
