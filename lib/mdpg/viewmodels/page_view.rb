0b96e9a7f379582158d46f263433d70dfb93186b
Gordon McCreight <gordon@mccreight.com>
2014-10-30 10:54:59 -0700

    Add table parsing

--- lib/mdpg/viewmodels/page_view.rb
+++ lib/mdpg/viewmodels/page_view.rb
@@ -35,7 +35,7 @@ class PageView < Struct.new(:user, :page, :token_type)
 
   def rendered_markdown(text)
     markdown = ::Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-      :autolink => true, :space_after_headers => true)
+      :autolink => true, :space_after_headers => true, :tables => true)
     markdown.render text
   end
 
