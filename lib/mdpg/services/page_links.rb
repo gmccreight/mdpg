f88563bfb27e5ca0f2fcb0df4e79e3bb4c162cbe
Gordon McCreight <gordon@mccreight.com>
2014-12-26 09:17:11 -0800

    refactor PageLinks

--- lib/mdpg/services/page_links.rb
+++ lib/mdpg/services/page_links.rb
@@ -1,21 +1,11 @@
 class PageLinks < Struct.new(:user)
 
   def internal_links_to_user_clickable_links text
-    return text if ! text
-    text.gsub(/\[\[mdpgpage:(\d+)\]\]/) do
-      id = $1.to_i
-      name = Page.find(id).name
-      "[#{name}](/p/#{name})"
-    end
+    altered_text text, mode: :user_clickable_link
   end
 
   def internal_links_to_page_name_links_for_editing text
-    return text if ! text
-    text.gsub(/\[\[mdpgpage:(\d+)\]\]/) do
-      id = $1.to_i
-      name = Page.find(id).name
-      "[[#{name}]]"
-    end
+    altered_text text, mode: :page_name_link_for_editing
   end
 
   def page_name_links_to_ids text
@@ -28,6 +18,19 @@ class PageLinks < Struct.new(:user)
         "[[mdpgpage:#{page.id}]]"
       else
         "[[#{$1}]]"
+      end
+    end
+  end
+
+  private def altered_text text, mode:
+    return text if ! text
+    text.gsub(/\[\[mdpgpage:(\d+)\]\]/) do
+      id = $1.to_i
+      name = Page.find(id).name
+      if mode == :user_clickable_link
+        "[#{name}](/p/#{name})"
+      elsif mode == :page_name_link_for_editing
+        "[[#{name}]]"
       end
     end
   end
