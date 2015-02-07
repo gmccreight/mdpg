a4aaf24d82012476ffc4dfc533d16899ad2f119d
Gordon McCreight <gordon@mccreight.com>
2015-02-07 10:23:08 -0800

    Track the ops stuff
    Only have the local config be untracked, and give example of how to add
    it.

---
+++ bin/ops/_shared.sh
@@ -0,0 +1,10 @@
+shared_local_config_path=`dirname $0`/_shared_local_config.sh
+
+if [ -f $shared_local_config_path ]; then
+  source $shared_local_config_path
+  has_local_config=1
+else
+  echo please create a file at $shared_local_config_path with variables like:
+  echo shared_mdpg_server=mdpg.co
+  echo shared_mdpg_remote_path="~/mdpg"
+fi
