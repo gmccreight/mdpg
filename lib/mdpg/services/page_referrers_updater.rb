7e9f9077828bd3ac844fb0eef1c0fe21cab0d664
Gordon McCreight <gordon@mccreight.com>
2015-05-23 13:05:54 -0700

    Refactor to use Ruby-ish array operations

--- lib/mdpg/services/page_referrers_updater.rb
+++ lib/mdpg/services/page_referrers_updater.rb
@@ -1,28 +1,26 @@
 class PageReferrersUpdater
 
   def add_page_id_to_referrers page_id, target_page
-    if target_page.has_any_referring_pages?
-      if ! target_page.referring_page_ids.include?(page_id)
-        ids = target_page.referring_page_ids
-        ids << page_id
-        ids = ids.sort
-        target_page.referring_page_ids = ids
-        target_page.save
-      end
-    else
+
+    if ! target_page.has_any_referring_pages?
       target_page.referring_page_ids = [page_id]
+      target_page.save
+      return
+    end
+
+    if ! target_page.referring_page_ids.include?(page_id)
+      target_page.referring_page_ids += [page_id]
+      target_page.referring_page_ids.sort!
       target_page.save
     end
   end
 
   def remove_page_id_from_referrers page_id, target_page
-    if target_page.has_any_referring_pages?
-      if target_page.referring_page_ids.include?(page_id)
-        target_page.referring_page_ids = target_page.referring_page_ids.
-          reject{|x| x == page_id}
-        target_page.save
-      end
-    end
+    return if ! target_page.has_any_referring_pages?
+    return if ! target_page.referring_page_ids.include?(page_id)
+
+    target_page.referring_page_ids -= [page_id]
+    target_page.save
   end
 
 end
