1c43d7aa20159de3a9a8655441b4eb20723dd84d
Gordon McCreight <gordon@mccreight.com>
2015-06-17 15:30:38 -0700

    fix indent

--- lib/mdpg/services/labeled_section_transcluder.rb
+++ lib/mdpg/services/labeled_section_transcluder.rb
@@ -19,54 +19,54 @@ class LabeledSectionTranscluder
     text.gsub(
       /\[\[mdpgpage:(\d+):(#{Token::TOKEN_REGEX_STR})\]\]/
     ) do
-        page_id = $1.to_i
-        section_identifier = $2
+      page_id = $1.to_i
+      section_identifier = $2
 
-        page = Page.find(page_id)
+      page = Page.find(page_id)
 
-        section = LabeledSections.new(Page.find(page_id).text)
-        section.process
+      section = LabeledSections.new(Page.find(page_id).text)
+      section.process
 
-        section_name = section.name_for(section_identifier)
+      section_name = section.name_for(section_identifier)
 
-        "[[#{page.name}##{section_name}:start]]
+      "[[#{page.name}##{section_name}:start]]
 
-        #{section.text_for(section_identifier)}
+      #{section.text_for(section_identifier)}
 
-        [[#{page.name}##{section_name}:end]]".gsub(/^[ ]+/, '')
-      end
+      [[#{page.name}##{section_name}:end]]".gsub(/^[ ]+/, '')
+    end
   end
 
   def user_facing_links_to_internal_links text, user_pages
     text = text.gsub(
       /\[\[(#{Token::TOKEN_REGEX_STR})#(#{Token::TOKEN_REGEX_STR})\]\]/
     ) do
-        page_name = $1
-        section_name = $2
+      page_name = $1
+      section_name = $2
 
-        result = ""
+      result = ""
 
-        if page_name == "mdpgpage"
-          result = "[[#{page_name}:#{section_name}]]"
-        else
-          page = user_pages.find_page_with_name(page_name)
+      if page_name == "mdpgpage"
+        result = "[[#{page_name}:#{section_name}]]"
+      else
+        page = user_pages.find_page_with_name(page_name)
 
-          if page
-            sections = LabeledSections.new(page.text)
-            sections.process
-            identifier = sections.identifier_for(section_name)
+        if page
+          sections = LabeledSections.new(page.text)
+          sections.process
+          identifier = sections.identifier_for(section_name)
 
-            if identifier
-              result = "[[mdpgpage:#{page.id}:#{identifier}]]"
-            end
+          if identifier
+            result = "[[mdpgpage:#{page.id}:#{identifier}]]"
           end
         end
+      end
 
-        if result == ""
-          result = "[[#{page_name}##{section_name}]]"
-        end
+      if result == ""
+        result = "[[#{page_name}##{section_name}]]"
+      end
 
-        result
+      result
     end
 
     text
@@ -77,28 +77,28 @@ class LabeledSectionTranscluder
     text = text.gsub(
       /\[\[mdpgpage:(\d+):(#{Token::TOKEN_REGEX_STR})\]\]/
     ) do
-        page_id = $1.to_i
-        section_id = $2
+      page_id = $1.to_i
+      section_id = $2
 
-        result = ""
+      result = ""
 
-        page = Page.find(page_id)
+      page = Page.find(page_id)
 
-        if page
-          sections = LabeledSections.new(page.text)
-          sections.process
-          section_name = sections.name_for(section_id)
+      if page
+        sections = LabeledSections.new(page.text)
+        sections.process
+        section_name = sections.name_for(section_id)
 
-          if section_name
-            result = "[[#{page.name}##{section_name}]]"
-          end
+        if section_name
+          result = "[[#{page.name}##{section_name}]]"
         end
+      end
 
-        if result == ""
-          result = "[[mdpgpage:#{page_id}##{section_id}]]"
-        end
+      if result == ""
+        result = "[[mdpgpage:#{page_id}##{section_id}]]"
+      end
 
-        result
+      result
     end
 
     text
