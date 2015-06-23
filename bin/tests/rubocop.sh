7de9fc34ec08e14160c53526d99829b7721d1740
Gordon McCreight <gordon@mccreight.com>
2015-06-22 17:11:42 -0700

    make the rubocop test even better

--- bin/tests/rubocop.sh
+++ bin/tests/rubocop.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-rubocop | egrep --color=always "[0-9]+ offenses detected"
+rm -f .rubocop_result
+rubocop --out .rubocop_result
+
+if egrep --quiet "[0-9]+ offenses detected" .rubocop_result; then
+  cat .rubocop_result
+
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+  printf "${RED}rubocop discovered some offenses!${NC}\n"
+fi
