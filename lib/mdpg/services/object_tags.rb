b6100b6d03208d3b44b7c949854f25cb7fb7d7ed
Gordon McCreight <gordon@mccreight.com>
2015-12-12 10:10:41 -0800

    use some caching in tags

--- lib/mdpg/services/object_tags.rb
+++ lib/mdpg/services/object_tags.rb
@@ -1,6 +1,18 @@
 require 'mdpg/token'
 
 class ObjectTags < Struct.new(:object)
+
+  def initialize(object)
+    reset_caches
+    super(object)
+  end
+
+  def reset_caches
+    @tags = []
+    @tags_cached_ids = []
+    @tag_with_name = {}
+  end
+
   def add_tag(name)
     return false if Token.new(name).validate
 
@@ -13,6 +25,7 @@ class ObjectTags < Struct.new(:object)
     tag.save
     object.add_tag tag
     object.save
+    reset_caches
   end
 
   def remove_tag(name)
@@ -32,17 +45,30 @@ class ObjectTags < Struct.new(:object)
   end
 
   def tag_with_name(name)
+    if @tag_with_name.key?(name)
+      return @tag_with_name[name]
+    end
     tags_with_name = tags.select { |tag| tag.name == name }
     if tags_with_name.size > 0
-      return tags_with_name.first
+      @tag_with_name[name] = tags_with_name.first
+      return @tag_with_name[name]
     else
+      @tag_with_name[name] = nil
       return nil
     end
   end
 
   def tags
     return [] unless object.tag_ids
-    object.tag_ids.map { |x| Tag.find(x) }
+
+    if @tags_cached_ids != object.tag_ids
+      @tags = object.tag_ids.map { |x| Tag.find(x) }
+      @tags_cached_ids = object.tag_ids
+      return @tags
+    else
+      # use the cached value
+      return @tags
+    end
   end
 
   def sorted_tag_names
