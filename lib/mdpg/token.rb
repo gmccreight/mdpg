a9f94b248d277212b34038a72849144dbedc840d
Gordon McCreight <gordon@mccreight.com>
2020-05-09 09:48:49 -0700

    Allow for tokens to be up to 64 long
    60 was pretty arbitrary, and surprising

--- lib/mdpg/token.rb
+++ lib/mdpg/token.rb
@@ -6,7 +6,7 @@ class Token < Struct.new(:text)
   def validate
     return :blank if !text || text.empty?
     return :too_short if text.size < 3
-    return :too_long if text.size > 60
+    return :too_long if text.size > 64
     return :only_a_z_0_9_and_hyphens_ok if text !~ /^#{TOKEN_REGEX_STR}$/
     nil
   end
