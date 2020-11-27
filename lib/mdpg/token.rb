c25795afef52fb3711833ec1d6a581fa5ad97cfe
Gordon McCreight <gordon@mccreight.com>
2020-11-27 09:09:42 -0800

    Use constants to define token length constraints

--- lib/mdpg/token.rb
+++ lib/mdpg/token.rb
@@ -2,11 +2,13 @@
 
 class Token < Struct.new(:text)
   TOKEN_REGEX_STR = '[a-z0-9-]+'
+  TOKEN_MIN_LENGTH = 3
+  TOKEN_MAX_LENGTH = 64
 
   def validate
     return :blank if !text || text.empty?
-    return :too_short if text.size < 3
-    return :too_long if text.size > 64
+    return :too_short if text.size < TOKEN_MIN_LENGTH
+    return :too_long if text.size > TOKEN_MAX_LENGTH
     return :only_a_z_0_9_and_hyphens_ok if text !~ /^#{TOKEN_REGEX_STR}$/
     nil
   end
